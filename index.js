b63ce5acc731ebf098bd34dba36111bdc0e2fa85
Andrew <SolomonAndrew@tutanota.com>
2022-06-29 11:08:00 +0300

    Static files configuration created.
    Template engine changed to 'pug',
    Paths for /img, /css, /js/ /fonts directories added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose"
-
+import path from "path";
 // 
 dotenv.config({
     path: './config/.env',
@@ -11,6 +11,15 @@ dotenv.config({
 const app = express();
 const PORT = process.env.PORT || 1337;
 const DB_CONNECT = process.env.DB_CONNECT;
+const __dirname = path.resolve();
+
+app.set("view engine", "pug");
+app.set("views", path.join(path.resolve(__dirname), '/static/views'));
+
+app.use("/img", express.static(path.join(__dirname, '/static/assets/img')))
+app.use("/css", express.static(path.join(__dirname, '/static/assets/css')))
+app.use("/js", express.static(path.join(__dirname, '/static/assets/js')))
+app.use("/fonts", express.static(path.join(__dirname, '/static/assets/fonts')))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
